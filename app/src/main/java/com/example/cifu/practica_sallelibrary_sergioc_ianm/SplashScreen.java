3c5c649c73a6211996c60006b7edc13c46ed5340
Sergio Cifuentes <cifupetit@gmail.com>
2018-02-14 19:26:18 +0100

    empezado splash screen de inicio

---
+++ app/src/main/java/com/example/cifu/practica_sallelibrary_sergioc_ianm/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.example.cifu.practica_sallelibrary_sergioc_ianm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by cifu on 14/02/2018.
+ */
+
+public class SplashScreen extends Activity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
