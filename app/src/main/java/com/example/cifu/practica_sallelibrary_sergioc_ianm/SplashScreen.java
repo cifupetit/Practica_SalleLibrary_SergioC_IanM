67420dab1263ebe4b62547de006d567481e8ccb6
Sergio Cifuentes <cifupetit@gmail.com>
2018-02-21 18:58:28 +0100

    SplashScreen OK - falta arreglo de textView por encima de imagen

--- app/src/main/java/com/example/cifu/practica_sallelibrary_sergioc_ianm/SplashScreen.java
+++ app/src/main/java/com/example/cifu/practica_sallelibrary_sergioc_ianm/SplashScreen.java
@@ -1,7 +1,9 @@
 package com.example.cifu.practica_sallelibrary_sergioc_ianm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 
@@ -11,9 +13,23 @@ import android.support.annotation.Nullable;
 
 public class SplashScreen extends Activity {
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Create an Intent that will start the MainActivity.
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
