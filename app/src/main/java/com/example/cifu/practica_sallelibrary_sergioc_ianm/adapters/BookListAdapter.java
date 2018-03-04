fb6992420c774ae7a37baf23dca85ffc014eecba
Sergio Cifuentes <cifupetit@gmail.com>
2018-03-04 18:56:16 +0100

    Book model, adapter ListBook y vista OK

---
+++ app/src/main/java/com/example/cifu/practica_sallelibrary_sergioc_ianm/adapters/BookListAdapter.java
@@ -0,0 +1,76 @@
+package com.example.cifu.practica_sallelibrary_sergioc_ianm.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.cifu.practica_sallelibrary_sergioc_ianm.R;
+import com.example.cifu.practica_sallelibrary_sergioc_ianm.models.BookModel;
+
+import java.util.List;
+
+/**
+ * Created by cifu on 04/03/2018.
+ */
+
+public class BookListAdapter extends BaseAdapter {
+    private Activity activity;
+    private List<BookModel> data;
+    private LayoutInflater inflater;
+    private int bookItemLayout;
+
+    public BookListAdapter(Activity activity, List<BookModel> data, int bookItemLayout) {
+        this.activity = activity;
+        this.data = data;
+        this.inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.bookItemLayout = bookItemLayout;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public BookModel getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //set custom layout
+        if (convertView == null) {convertView = this.inflater.inflate(this.bookItemLayout, null);}
+        //get views from custom layout
+        //ImageView imgLibro = (ImageView) convertView.findViewById(R.id.imagen_book_list);
+        TextView booTitle = (TextView) convertView.findViewById(R.id.titulo_book_list);
+        TextView bookSubtitle = (TextView) convertView.findViewById(R.id.subtitulo_book_list);
+        TextView bookAuthors = (TextView) convertView.findViewById(R.id.autores_book_list);
+        TextView bookPublisher = (TextView) convertView.findViewById(R.id.editorial_book_list);
+        TextView bookDate = (TextView) convertView.findViewById(R.id.fecha_book_list);
+        TextView bookPrice = (TextView) convertView.findViewById(R.id.precio_book_list);
+        TextView bookDescription = (TextView) convertView.findViewById(R.id.descripcion_book_list);
+
+        //set values for item at position
+        Glide.with(this.activity).load(data.get(position).getImg()).into((ImageView) convertView.findViewById(R.id.imagen_book_list));
+        booTitle.setText(data.get(position).getTitle());
+        bookSubtitle.setText(data.get(position).getSubtitle());
+        bookAuthors.setText(data.get(position).getAuthors().toString());
+        bookPublisher.setText(data.get(position).getPublisher());
+        bookDate.setText(data.get(position).getPublishDate());
+        bookPrice.setText(String.valueOf(data.get(position).getPrice()));
+        bookDescription.setText(data.get(position).getDescription());
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/cifu/practica_sallelibrary_sergioc_ianm/models/BookModel.java
@@ -0,0 +1,130 @@
+package com.example.cifu.practica_sallelibrary_sergioc_ianm.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by cifu on 03/03/2018.
+ */
+
+public class BookModel implements Parcelable {
+    private String img, title , subtitle, description, publisher;
+    private String[] authors;
+    private String publishDate;
+    private double price;
+
+    public BookModel(String img, String title, String subtitle, String description, String publisher, String[] authors, String publishDate, double price) {
+        this.img = img;
+        this.title = title;
+        this.subtitle = subtitle;
+        this.description = description;
+        this.publisher = publisher;
+        this.authors = authors;
+        this.publishDate = publishDate;
+        this.price = price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.img);
+        dest.writeString(this.title);
+        dest.writeString(this.subtitle);
+        dest.writeString(this.description);
+        dest.writeString(this.publisher);
+        dest.writeStringArray(this.authors);
+        dest.writeString(this.publishDate);
+        dest.writeDouble(this.price);
+    }
+
+    public static final Parcelable.Creator<BookModel> CREATOR = new Parcelable.Creator<BookModel>() {
+        @Override
+        public BookModel createFromParcel(Parcel source) {
+            return new BookModel(source);
+        }
+
+        @Override
+        public BookModel[] newArray(int size) {
+            return new BookModel[size];
+        }
+    };
+
+    private BookModel (Parcel source) {
+        this.img = source.readString();
+        this.title = source.readString();
+        this.subtitle = source.readString();
+        this.description = source.readString();
+        this.publisher = source.readString();
+        source.readStringArray(this.authors);
+        this.publishDate = source.readString();
+        this.price = source.readDouble();
+    }
+}
